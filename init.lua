3af4e21c7e52a2dd9fe4e71b8a9e2d4967c894ce
Magomed Bakhmadov <magomed.k98@gmail.com>
2024-02-04 18:48:16 +0100

    Moved plugins into their own files and also installed lualine.

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.g.mapleader = " "
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,30 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-local configs = require("nvim-treesitter.configs")
-
-configs.setup({
-    ensure_installed = {"lua", "vim", "vimdoc", "rust"},
-    highlight = { enable = true },
-    indent = { enable = true },
-})
+require("options")
+require("lazy").setup("plugins")
